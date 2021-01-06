0ebffbe392a94066f9561c11680a7df00ae74546
joojoo <joojoo@DESKTOP-JR5CPIF>
2021-01-06 16:39:37 +0900

    spring DI

--- src/test/java/orz/zerock/sample/SampleTests.java
+++ src/test/java/orz/zerock/sample/SampleTests.java
@@ -1,5 +1,6 @@
 package orz.zerock.sample;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -9,17 +10,36 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class SampleTests {
+    @Autowired
+   private ApplicationContext context;
+    
+    @Setter(onMethod_ = {@Autowired})
+    private Restaurant restaurant;
+	   public void testExist() {
+	   assertNotNull(restaurant);
+	   
+	   log.info(restaurant);
+	   log.info("-------------");
+	   log.info(restaurant.getChef());
+	   }
+	
       
-       @Autowired
-      private ApplicationContext context;
+
       @Test
-      public void testExist() {
+      public void testExist2() {
+    	  Chef c1 = (Chef) context.getBean("chef");
+    	  Restaurant r1 = (Restaurant) context.getBean("restaurant");
+    	  
+    	  assertNotNull(r1.getChef());
+    	  assertEquals(c1, r1.getChef());
+    	  
          log.info("----------");
          assertNotNull(context);
          assertNotNull(context.getBean("chef"));
